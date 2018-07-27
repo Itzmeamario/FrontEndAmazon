2b86435b6afa9b3d17eeccc9d11a410560478171
Mario <mario.santamaria.portilla@gmail.com>
2018-07-27 12:14:02 -0700

    included css style loader

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import style from '../styles/App.css'
 
 class App extends Component {
   constructor(props) {
@@ -24,31 +25,38 @@ class App extends Component {
     return (
       <div>
         <hr/>
-          <div className="grid-container">
-            <div className="totalizer">
-              <div className="customerReviews">Customer reviews</div>
-              <div className="topCostumer">Top customer reviews</div>
+          <div className={style.gridContainer}>
+            <div className={style.totalizer}>
+              <div className={style.customerReviews}>Customer reviews</div>
+              <div className={style.topCostumer}>Top customer reviews</div>
             </div>
-            <div className="mostRecent">
-              <div className="customerImages">
-                Customer images
+            <div className={style.mostRecent}>
+              <div className={style.customerImages}>
+                <div className={style.imagesTitle}>Customer images</div>
+                <div className={style.imageContainer}>
+                  <img className={style.imageDisplay} src="https://s3.amazonaws.com/uifaces/faces/twitter/panghal0/128.jpg" alt="Avatar"/>
+                  <img className={style.imageDisplay} src="https://s3.amazonaws.com/uifaces/faces/twitter/panghal0/128.jpg" alt="Avatar"/>
+                  <img className={style.imageDisplay} src="https://s3.amazonaws.com/uifaces/faces/twitter/panghal0/128.jpg" alt="Avatar"/>
+                  <img className={style.imageDisplay} src="https://s3.amazonaws.com/uifaces/faces/twitter/panghal0/128.jpg" alt="Avatar"/>
+                </div>
               </div>
-              <div className="recentReviews">
-                Most recent customer reviews
-                <div className="review">
-                  <div className="avatarHolder">
-                    <img className="avatarImage" src="https://s3.amazonaws.com/uifaces/faces/twitter/panghal0/128.jpg" alt="Avatar"/>
-                    <text className="avatarUsername">Mario Santamaria</text>
+              <div className={style.recentReviews}>
+                <div className={style.recentTitle}>Most recent customer reviews</div>
+                <div className={style.review}>
+                  <div className={style.avatarHolder}>
+                    <img className={style.avatarImage} src="https://s3.amazonaws.com/uifaces/faces/twitter/panghal0/128.jpg" alt="Avatar"/>
+                    <text className={style.avatarUsername}>Mario Santamaria</text>
                   </div>
-                  <div className="reviewData">
-                    <a className="reviewText">I love it! I use it to play music and I also use it to turn on/off my Sengled Smart Bulb.I don't know why I waited so long to get one of these, but I will be ordering one for my parent's home also.</a>
+                  <div className={style.reviewData}>
+                    <a className={style.reviewText}>I love it! I use it to play music and I also use it to turn on/off my Sengled Smart Bulb.I don't know why I waited so long to get one of these, but I will be ordering one for my parent's home also.</a>
+
                   </div>
                 </div>
               </div>
-              <div className="search">
+              <div className={style.search}>
                 Search customer reviews
                 <form>
-                  <input className="searchReviews" type="text" name="search"/>
+                  <input className={style.searchReviews} type="text" name="search"/>
                 </form>
               </div>
             </div>
